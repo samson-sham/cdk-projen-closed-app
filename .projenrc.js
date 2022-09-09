0f50b947bb552b539e76e107a1180cacd47b0479
Samson Sham <>
2022-09-10 00:03:23 +0900

    feat: feature complete

--- .projenrc.js
+++ .projenrc.js
@@ -1,14 +1,22 @@
-const { cdk } = require('projen');
+const { cdk, javascript } = require('projen');
 const project = new cdk.JsiiProject({
-  author: 'user',
-  authorAddress: 'user@domain.com',
+  author: 'Samson Sham',
+  authorAddress: '',
   defaultReleaseBranch: 'main',
-  name: 'projen',
-  repositoryUrl: 'https://github.com/user/projen.git',
+  name: 'cdk-projen-closed-app',
+  repositoryUrl: 'https://github.com/samson-sham/cdk-projen-closed-app.git',
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
+  packageManager: javascript.NodePackageManager.PNPM,
+  depsUpgrade: false,
+  peerDeps: [
+    'projen',
+  ],
+});
+project.eslint.addRules({
+    '@typescript-eslint/indent': ['error', 4],
 });
 project.synth();
